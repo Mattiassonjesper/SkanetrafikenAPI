1d67ed86daf586e9208e52935ae5eb44e281b57f
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-01-27 14:21:19 +0100

    Structure

--- src/grit/skane/StationServlet.java
+++ src/grit/skane/StationServlet.java
@@ -28,111 +28,105 @@ import org.xml.sax.InputSource;
 @WebServlet("/StationServlet")
 public class StationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public StationServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public StationServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+
 		response.setContentType("text/html");
-		
+
 		PrintWriter out = response.getWriter();
-		
+
 		out.print("<br>");
-		
-				// Build the API call by adding x and y into a URL
-				String x = "6167817", y = "1323658";
-						String NewURL = "http://www.labs.skanetrafiken.se/v2.2/neareststation.asp?x="
-								+ x + "&y=" + y + "&Radius=500";
 
-						System.out.println(NewURL);
+		// Build the API call by adding x and y into a URL
+		String x = "6167817", y = "1323658";
+		String NewURL = "http://www.labs.skanetrafiken.se/v2.2/neareststation.asp?x=" + x + "&y=" + y + "&Radius=500";
 
-						// Set the URL that will be sent
-						URL StationURL = new URL(NewURL);
+		System.out.println(NewURL);
 
-						// Create a HTTP connection to sent the GET request over
-						HttpURLConnection linec = (HttpURLConnection) StationURL.openConnection();
-						linec.setDoInput(true);
-						linec.setDoOutput(true);
-						linec.setRequestMethod("GET");
+		// Set the URL that will be sent
+		URL StationURL = new URL(NewURL);
 
-						// Make a Buffer to read the response from the API
-						BufferedReader in = new BufferedReader(new InputStreamReader(linec.getInputStream()));
+		// Create a HTTP connection to sent the GET request over
+		HttpURLConnection linec = (HttpURLConnection) StationURL.openConnection();
+		linec.setDoInput(true);
+		linec.setDoOutput(true);
+		linec.setRequestMethod("GET");
 
-						// a String to temp save each line in the response
-						String inputLine;
+		// Make a Buffer to read the response from the API
+		BufferedReader in = new BufferedReader(new InputStreamReader(linec.getInputStream()));
 
-						// a String to save the full response to use later
-						String ApiResponse = "";
+		// a String to temp save each line in the response
+		String inputLine;
 
-						// loop through the whole response
-						while ((inputLine = in.readLine()) != null) {
-							
-							//System.out.println(inputLine);
-							// Save the temp line into the full response
-							ApiResponse += inputLine;
-						}
-						in.close();
-						System.out.println(ApiResponse);
-						
-						
-						//Call a method to make a XMLdoc out of the full response
-						Document doc = convertStringToXMLDocument(ApiResponse);
+		// a String to save the full response to use later
+		String ApiResponse = "";
 
-						
-						//normalize the XML response
-						doc.getDocumentElement().normalize();
-						// check that the XML response is OK by getting the Root element 
-						System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		// loop through the whole response
+		while ((inputLine = in.readLine()) != null) {
 
-						// Create a Node list that gets everything in and under the "clouds" tag  
-						NodeList nList = doc.getElementsByTagName("NearestStopArea");
-						
-						System.out.println(nList.getLength());
-						// loop through the content of the tag
-						for (int temp = 0; temp < nList.getLength(); temp++) {
-							// Save a node of the current list id 
-							Node node = nList.item(temp);
-							
-							if (node.getNodeType() == Node.ELEMENT_NODE) {
+			// System.out.println(inputLine);
+			// Save the temp line into the full response
+			ApiResponse += inputLine;
+		}
+		in.close();
+		System.out.println(ApiResponse);
 
-								// set the current node as an Element
-								Element eElement = (Element) node;
-								// get the content of an attribute in element
-								// and print it out to the client 
-								out.print("Nearest stop " + eElement.getElementsByTagName("Name")
-															.item(0).getTextContent());
-								out.print(" Id: " + eElement.getElementsByTagName("Id")
-								  							.item(0).getTextContent());
-								out.println(" Distance: " + eElement.getElementsByTagName("Distance")
-															  .item(0).getTextContent());
+		// Call a method to make a XMLdoc out of the full response
+		Document doc = convertStringToXMLDocument(ApiResponse);
 
-						
-							}
-						}
+		// normalize the XML response
+		doc.getDocumentElement().normalize();
+		// check that the XML response is OK by getting the Root element
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 
-					}
-	
+		// Create a Node list that gets everything in and under the "clouds" tag
+		NodeList nList = doc.getElementsByTagName("NearestStopArea");
+
+		System.out.println(nList.getLength());
+		// loop through the content of the tag
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			// Save a node of the current list id
+			Node node = nList.item(temp);
+
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+				// set the current node as an Element
+				Element eElement = (Element) node;
+				// get the content of an attribute in element
+				// and print it out to the client
+				out.print("Nearest stop " + eElement.getElementsByTagName("Name").item(0).getTextContent());
+				out.print(" Id: " + eElement.getElementsByTagName("Id").item(0).getTextContent());
+				out.println(" Distance: " + eElement.getElementsByTagName("Distance").item(0).getTextContent());
+
+			}
+		}
+
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
-
 
 //Method the makes a XML doc out of a string, if it is in a XML format. 	
 	private static Document convertStringToXMLDocument(String xmlString) {
